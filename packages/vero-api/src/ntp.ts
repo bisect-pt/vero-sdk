51af2ea65c9b441d35eb1d18db756a569b0a8c19
Pedro Ferreira <pedro@bisect.pt>
2021-05-10 15:45:42 +0000

    Merged PR 728: Update NTP settings
    NTP settings

--- packages/vero-api/src/ntp.ts
+++ packages/vero-api/src/ntp.ts
@@ -4,6 +4,10 @@ export interface INtpStatus {
 }
 
 export interface INtpSettings {
+    // If true, use the server provided by DHCP or the default NTP server.
+    // If false, use the one defined in the serverAddress property
     useSystem: boolean;
-    serverAddresses?: string[];
+
+    // A list of space-separated addresses for NTP servers.
+    serverAddress?: string;
 }
