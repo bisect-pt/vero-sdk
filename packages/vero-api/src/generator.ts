5e26fa34bfdea64eefda08203313dcda487b70fa
Pedro Ferreira <pedro@bisect.pt>
2021-06-17 14:34:00 +0000

    Merged PR 765: Enable/disable moving bar
    Enable/disable moving bar

--- packages/vero-api/src/generator.ts
+++ packages/vero-api/src/generator.ts
@@ -47,6 +47,7 @@ export interface IVideoSettings {
         distribution_mode?: string;
     };
     patternId: string;
+    enableMovingBar: boolean;
 }
 export interface IAncSettings {
     //TODO
